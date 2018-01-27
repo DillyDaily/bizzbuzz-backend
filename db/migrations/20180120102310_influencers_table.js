0bfbf10a9386e140ba201e39732a84b7037eaa59
Alexis <ali.finney@gmail.com>
2018-01-27 09:06:58 -0700

    changed db for buzz

--- db/migrations/20180120102310_influencers_table.js
+++ db/migrations/20180120102310_influencers_table.js
@@ -1,16 +1,16 @@
 exports.up = function(knex, Promise) {
     return knex.schema.createTable('influencers', function (table) {
         table.increments();
+        table.text('email');
+        table.text('password');
         table.text('first_name');
         table.text('last_name');
         table.text('description');
-        table.text('email');
-        table.text('password');
+        table.text('topics');
         table.text('personal_brand');
-        table.text('website');
-        table.text('facebook');
-        table.text('instagram');
-        table.text('twitter');
+        table.text('category');
+        table.text('city');
+        table.text('state');
         table.text('image');
         table.timestamps(true, true);
         });
--- db/seeds/influencers.js
+++ db/seeds/influencers.js
@@ -5,9 +5,9 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('influencers').insert([
-        {personal_brand: 'brand1', first_name: 'fname1', last_name: 'lname1', description: 'this is a description1', email: 'email@email.com1', password: 'password1', image: 'image1', website: 'site1', facebook: 'facebook1', instagram: 'instagram1', twitter: 'tweet1' },
-        {personal_brand: 'brand2', first_name: 'fname2', last_name: 'lname2', description: 'this is a description2', email: 'email@email.com2', password: 'password2', image: 'image2', website: 'site2', facebook: 'facebook2', instagram: 'instagram2', twitter: 'tweet2' },
-        {personal_brand: 'brand3', first_name: 'fname3', last_name: 'lname3', description: 'this is a description3', email: 'email@email.com3', password: 'password3', image: 'image3', website: 'site3', facebook: 'facebook3', instagram: 'instagram3', twitter: 'tweet3' }
+        {personal_brand: 'brand1', first_name: 'fname1', last_name: 'lname1', description: 'this is a very short description describing things about the buzz1', email: 'email@email.com1', password: 'password1', image: 'image1', topics: 'topic1, stuff, things, cats, dogs', category: 'category1', city: 'Phoenix', state: 'AZ' },
+        {personal_brand: 'brand2', first_name: 'fname2', last_name: 'lname2', description: 'this is a very short description describing things about the buzz2', email: 'email@email.com2', password: 'password2', image: 'image2', topics: 'topic2, other, more, birds, hamsters', category: 'category2', city: 'Denver', state: 'CO' },
+        {personal_brand: 'brand3', first_name: 'fname3', last_name: 'lname3', description: 'this is a very short description describing things about the buzz3', email: 'email@email.com3', password: 'password3', image: 'image3', topics: 'topic3, stuff, things, cats, dogs', category: 'category3', city: 'Boston', state: 'MA' }
       ]);
     });
 };
